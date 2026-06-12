713ac2586e9b5565de19d84d9050034c3ea35559
louisher <louis.hermans@kuleuven.be>
2026-06-12 13:34:38 +0200

    update

--- COSINE/input_data/write_profilespy.py
+++ COSINE/input_data/write_profilespy.py
@@ -11,13 +11,13 @@ import sys
 module_path = r"C:\Workdir\Develop\IOCS"
 if module_path not in sys.path:
     sys.path.append(module_path)
-import TACO_functions as uf
-import helper_functions as hf
+import COSINE.utils.TACO_functions as uf
+import COSINE.utils.helper_functions as hf
 from pathlib import Path
 
 
-path_input_data = Path(r"C:\Workdir\Develop\IOCS\input_data").resolve()
-path_results = Path(r"C:\Users\u0148284\OneDrive - KU Leuven\Taco\ResultFiles\IOCS\25250415_ResultsAfterBorefieldFix\OneHouse_TMY\Iteration0").resolve()
+path_input_data = Path(r"C:\Workdir\Develop\IOCShub\COSINE\input_data").resolve()
+path_results = Path(r"C:\Users\u0148284\OneDrive - KU Leuven\PhD\FinalResults\TwoStepIOCS\MixedUse_TMY\Iteration4").resolve()
 
 writing_time = hf.write_input_profiles(
     path_results, path_input_data)
